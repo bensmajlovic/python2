618087ba4833c36da0bc4a56a251dfcf116aec2d
Ben <benjamin.smajlovic@gmail.com>
2018-08-07 16:12:02 -0400

    added exercise3.5.py

---
+++ Exercise3.5.py
@@ -0,0 +1,17 @@
+def first_line():
+	print('+ - - - - + - - - - +')
+
+def other_line():
+	print('| | |')
+
+first_line()
+other_line()
+other_line()
+other_line()
+other_line()
+first_line()
+other_line()
+other_line()
+other_line()
+other_line()
+first_line()
